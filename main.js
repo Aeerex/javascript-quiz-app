5c0601a52572fe65643e1797289e111a4dcb22c2
Aeerex <61473747+Aeerex@users.noreply.github.com>
2020-09-19 14:19:19 -0700

    JavaScript Quiz game
    JavaScript quiz game

---
+++ main.js
@@ -0,0 +1,16 @@
+function Quiz(questions){
+    this. scores = 0;
+    this. questions = questions;
+    this. questionIndex = 0;
+}
+class Quiz{
+    constructor(){
+        this. scores = 0;
+        this. questions = questions;
+        this. questionIndex = 0; 
+    }
+    getquestionIndex (){
+        return this.questions[this.questionIndex];
+    }
+    
+}
